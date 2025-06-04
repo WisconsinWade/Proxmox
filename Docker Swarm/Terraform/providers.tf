d3048c8d4f5099f9001ead51e38f5b2c8759eb13
WisconsinWade <129554089+WisconsinWade@users.noreply.github.com>
2025-06-04 15:46:09 -0500

    Create providers.tf

---
+++ Docker Swarm/Terraform/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "= 3.0.2-rc01"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_tls_insecure     = true  
+  pm_api_url          = "" # The Proxmox URL
+  pm_api_token_id     = ""# The Proxmox Terraform APIUser ID
+  pm_api_token_secret = ""# The Proxmox Terraform APIUser secret
+}
